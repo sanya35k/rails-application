1ca9550b5284cd62e4a24df0bc09f2fea5a70f32
sanya35k <serhii.saltan@gotoinc.co>
2021-12-23 12:01:10 +0200

    TeachBase Lesson10 Created HomePage, RailwayStation and Train using scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :trains
+  resources :railway_stations
+
+  get 'home/index'
+
+  root 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
